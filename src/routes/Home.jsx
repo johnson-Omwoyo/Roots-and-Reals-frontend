852ceac2b6284306c31a73cd32350f384beef1e3
Johnson Omwoyo <johnsonomwoyo100@gmail.com>
2024-10-27 16:23:00 +0300

    make image display full on sm and m-md-5

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -5,7 +5,7 @@ function Home() {
   return (
     <div className=" container-fluid ">
       <div className="row">
-        <div className="col p-0 m-5">
+        <div className="col p-0 m-md-5">
           <img
             src="https://images.unsplash.com/photo-1542601906990-b4d3fb778b09?q=80&w=2026&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
             alt=""
